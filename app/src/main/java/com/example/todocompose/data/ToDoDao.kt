08a16e50cfa59554c729b58e3e809be1c6b3617c
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2023-11-22 21:10:19 +0900

    addTaskを追加

--- app/src/main/java/com/example/todocompose/data/ToDoDao.kt
+++ app/src/main/java/com/example/todocompose/data/ToDoDao.kt
@@ -1,6 +1,8 @@
 package com.example.todocompose.data
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.todocompose.data.models.ToDoTask
 import kotlinx.coroutines.flow.Flow
@@ -14,4 +16,7 @@ interface ToDoDao {
 
   @Query("SELECT * FROM todo_table WHERE id=:taskId")
   fun getSelectedTask(taskId: Int): Flow<ToDoTask>
+
+  @Insert(onConflict = OnConflictStrategy.IGNORE)
+  suspend fun addTask(toDoTask: ToDoTask)
 }
