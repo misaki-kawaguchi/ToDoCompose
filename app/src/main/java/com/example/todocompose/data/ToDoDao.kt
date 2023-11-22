b4fa482c2c4c7dc5fd564b6a67c11258dfdf72f7
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2023-11-22 21:22:15 +0900

    sortByLowPriorityを追加

--- app/src/main/java/com/example/todocompose/data/ToDoDao.kt
+++ app/src/main/java/com/example/todocompose/data/ToDoDao.kt
@@ -28,4 +28,7 @@ interface ToDoDao {
 
   @Query("SELECT * FROM todo_table WHERE title LIKE :searchQuery OR description LIKE :searchQuery")
   fun searchDatabase(searchQuery: String): Flow<List<ToDoTask>>
+
+  @Query("SELECT * FROM todo_table ORDER BY CASE WHEN priority LIKE 'L%' THEN 1 WHEN priority LIKE 'M%' THEN 2 WHEN priority LIKE 'H%' THEN 3 END")
+  fun sortByLowPriority(): Flow<List<ToDoTask>>
 }
