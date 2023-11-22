7ff0e8d9fe74989a7598b131f9031080c31b47b2
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2023-11-22 21:04:37 +0900

    getAllTasksを追加

--- app/src/main/java/com/example/todocompose/data/ToDoDao.kt
+++ app/src/main/java/com/example/todocompose/data/ToDoDao.kt
@@ -1,8 +1,14 @@
 package com.example.todocompose.data
 
 import androidx.room.Dao
+import androidx.room.Query
+import com.example.todocompose.data.models.ToDoTask
+import kotlinx.coroutines.flow.Flow
 
 // データアクセスオブジェクト
 @Dao
 interface ToDoDao {
+
+  @Query("SELECT * FROM todo_table ORDER BY id ASC")
+  fun getAllTasks(): Flow<List<ToDoTask>>
 }
