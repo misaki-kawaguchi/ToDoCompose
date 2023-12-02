683665f6ff2f269a10f33ac7811324519c55dcf1
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2023-12-02 18:45:05 +0900

    微修正

--- app/src/main/java/com/example/todocompose/navigation/destinations/ListComposable.kt
+++ app/src/main/java/com/example/todocompose/navigation/destinations/ListComposable.kt
@@ -4,15 +4,14 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
-import com.example.todocompose.util.Constants.LIST_ARGUMENT_KEY
-import com.example.todocompose.util.Constants.LIST_SCREEN
+import com.example.todocompose.util.Constants
 
 fun NavGraphBuilder.listComposable(
   navigateToTaskScreen: (Int) -> Unit,
 ) {
   composable(
-    route = LIST_SCREEN,
-    arguments = listOf(navArgument(LIST_ARGUMENT_KEY) {
+    route = Constants.LIST_SCREEN,
+    arguments = listOf(navArgument(Constants.LIST_ARGUMENT_KEY) {
       type = NavType.StringType
     })
   ) {
