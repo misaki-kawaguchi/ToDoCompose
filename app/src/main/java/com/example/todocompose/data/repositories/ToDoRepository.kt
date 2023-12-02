d97de2023e7486e68317ee2a2cc998984d6629bb
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2023-12-02 15:54:40 +0900

    getAllTasksを追加

--- app/src/main/java/com/example/todocompose/data/repositories/ToDoRepository.kt
+++ app/src/main/java/com/example/todocompose/data/repositories/ToDoRepository.kt
@@ -1,7 +1,11 @@
 package com.example.todocompose.data.repositories
 
 import com.example.todocompose.data.ToDoDao
+import com.example.todocompose.data.models.ToDoTask
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class ToDoRepository @Inject constructor(private val toDoDao: ToDoDao) {
+
+  val getAllTasks: Flow<List<ToDoTask>> = toDoDao.getAllTasks()
 }
