1b6713f2f6223ae654f3f2b1b02cad54dbf1c957
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2023-12-02 15:57:35 +0900

    getSelectedTaskを追加

--- app/src/main/java/com/example/todocompose/data/repositories/ToDoRepository.kt
+++ app/src/main/java/com/example/todocompose/data/repositories/ToDoRepository.kt
@@ -10,4 +10,8 @@ class ToDoRepository @Inject constructor(private val toDoDao: ToDoDao) {
   val getAllTasks: Flow<List<ToDoTask>> = toDoDao.getAllTasks()
   val sortByLowPriority: Flow<List<ToDoTask>> = toDoDao.sortByLowPriority()
   val sortByHighPriority: Flow<List<ToDoTask>> = toDoDao.sortByHighPriority()
+
+  fun getSelectedTask(taskId: Int): Flow<ToDoTask> {
+    return toDoDao.getSelectedTask(taskId = taskId)
+  }
 }
