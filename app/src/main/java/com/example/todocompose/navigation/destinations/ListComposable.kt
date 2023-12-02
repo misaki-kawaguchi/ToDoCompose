80b4c811f5da6219aab6ebf1f6eb6d6d55a90f31
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2023-12-02 18:53:06 +0900

    微修正

--- app/src/main/java/com/example/todocompose/navigation/destinations/ListComposable.kt
+++ app/src/main/java/com/example/todocompose/navigation/destinations/ListComposable.kt
@@ -4,14 +4,15 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
-import com.example.todocompose.util.Constants
+import com.example.todocompose.util.Constants.LIST_ARGUMENT_KEY
+import com.example.todocompose.util.Constants.LIST_SCREEN
 
 fun NavGraphBuilder.listComposable(
   navigateToTaskScreen: (Int) -> Unit,
 ) {
   composable(
-    route = Constants.LIST_SCREEN,
-    arguments = listOf(navArgument(Constants.LIST_ARGUMENT_KEY) {
+    route = LIST_SCREEN,
+    arguments = listOf(navArgument(LIST_ARGUMENT_KEY) {
       type = NavType.StringType
     })
   ) {
--- app/src/main/java/com/example/todocompose/navigation/destinations/TaskComposable.kt
+++ app/src/main/java/com/example/todocompose/navigation/destinations/TaskComposable.kt
@@ -5,14 +5,15 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.example.todocompose.util.Action
-import com.example.todocompose.util.Constants
+import com.example.todocompose.util.Constants.TASK_ARGUMENT_KEY
+import com.example.todocompose.util.Constants.TASK_SCREEN
 
 fun NavGraphBuilder.taskComposable(
   navigateToListScreen: (Action) -> Unit
 ) {
   composable(
-    route = Constants.TASK_SCREEN,
-    arguments = listOf(navArgument(Constants.TASK_ARGUMENT_KEY) {
+    route = TASK_SCREEN,
+    arguments = listOf(navArgument(TASK_ARGUMENT_KEY) {
       type = NavType.IntType
     })
   ) {
