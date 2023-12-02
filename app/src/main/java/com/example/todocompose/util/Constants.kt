7960da9107d18ac4a255157a0a5adf7cecffde6d
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2023-12-02 18:23:20 +0900

    listを作成

--- app/src/main/java/com/example/todocompose/util/Constants.kt
+++ app/src/main/java/com/example/todocompose/util/Constants.kt
@@ -3,4 +3,6 @@ package com.example.todocompose.util
 object Constants {
   const val DATABASE_TABLE = "todo_table"
   const val DATABASE_NAME = "todo_database"
+
+  const val LIST_SCREEN = "list/{action}"
 }
