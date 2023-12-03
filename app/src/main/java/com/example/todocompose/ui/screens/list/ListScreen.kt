d294225ae4837dfa56e21f17c33dcad1bc2fd85c
misaki kawaguchi <60394359+misaki-kawaguchi@users.noreply.github.com>
2023-12-03 19:02:30 +0900

    List Screen - FAB (#9)
    * ListScreenを作成
    
    * 引数を追加
    
    * Previewを追加
    
    * Scaffoldを追加
    
    * ListFabを作成・表示

---
+++ app/src/main/java/com/example/todocompose/ui/screens/list/ListScreen.kt
@@ -0,0 +1,49 @@
+package com.example.todocompose.ui.screens.list
+
+import android.annotation.SuppressLint
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.todocompose.R
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun ListScreen(
+  navigateToTaskScreen: (Int) -> Unit,
+) {
+  Scaffold(
+    content = {},
+    floatingActionButton = {
+      ListFab(OnFabClicked = navigateToTaskScreen)
+    }
+  )
+}
+
+@Composable
+fun ListFab(
+  OnFabClicked: (Int) -> Unit,
+) {
+  FloatingActionButton(onClick = {
+    OnFabClicked(-1)
+  }) {
+    Icon(
+      imageVector = Icons.Filled.Add,
+      contentDescription = stringResource(
+        id = R.string.add_button
+      ),
+      tint = Color.White,
+    )
+  }
+}
+
+@Composable
+@Preview
+private fun ListScreenPreview() {
+  ListScreen(navigateToTaskScreen = {})
+}
