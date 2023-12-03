17254780fbb91686b02302e0f2dbf0e2d014a4c3
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2023-12-03 18:51:39 +0900

    Scaffoldを追加

--- app/src/main/java/com/example/todocompose/ui/screens/list/ListScreen.kt
+++ app/src/main/java/com/example/todocompose/ui/screens/list/ListScreen.kt
@@ -1,13 +1,20 @@
 package com.example.todocompose.ui.screens.list
 
+import android.annotation.SuppressLint
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun ListScreen(
   navigateToTaskScreen: (Int) -> Unit,
 ) {
-
+  Scaffold(
+    content = {},
+    floatingActionButton = {
+    }
+  )
 }
 
 @Composable
