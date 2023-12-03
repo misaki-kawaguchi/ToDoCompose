3dff8de2d18a09b88d26bf8190fbeb35f6f9bb64
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2023-12-03 19:00:54 +0900

    ListFabを作成・表示

--- app/src/main/java/com/example/todocompose/ui/screens/list/ListScreen.kt
+++ app/src/main/java/com/example/todocompose/ui/screens/list/ListScreen.kt
@@ -1,9 +1,16 @@
 package com.example.todocompose.ui.screens.list
 
 import android.annotation.SuppressLint
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.todocompose.R
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
@@ -13,8 +20,26 @@ fun ListScreen(
   Scaffold(
     content = {},
     floatingActionButton = {
+      ListFab(OnFabClicked = navigateToTaskScreen)
     }
   )
+}
+
+@Composable
+fun ListFab(
+  OnFabClicked: (Int) -> Unit,
+) {
+  FloatingActionButton(onClick = {
+    OnFabClicked(-1)
+  }) {
+    Icon(
+      imageVector = Icons.Filled.Add,
+      contentDescription = stringResource(
+        id = R.string.add_button
+      ),
+      tint = Color.White,
+    )
+  }
 }
 
 @Composable
