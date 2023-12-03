9c72ba18b5d1d53e818d466c239e9aa93ea24aab
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2023-12-03 18:44:31 +0900

    Previewを追加

--- app/src/main/java/com/example/todocompose/ui/screens/list/ListScreen.kt
+++ app/src/main/java/com/example/todocompose/ui/screens/list/ListScreen.kt
@@ -1,10 +1,17 @@
 package com.example.todocompose.ui.screens.list
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun ListScreen(
   navigateToTaskScreen: (Int) -> Unit,
 ) {
 
+}
+
+@Composable
+@Preview
+private fun ListScreenPreview() {
+  ListScreen(navigateToTaskScreen = {})
 }
